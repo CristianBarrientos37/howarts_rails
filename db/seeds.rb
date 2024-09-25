9a4ae349b15c87888e1b4e914efd871490912b76
CristianBarrientos37 <cristiantj_cbm@hotmail.com>
2024-09-25 11:41:53 -0300

    ahora si

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,5 +10,6 @@
 
 
 20.times do |i|
-  Character.create(name: Faker::Movies::HarryPotter.character, location: Faker::Movies::HarryPotter.location, house: Faker::Movies::HarryPotter.house)
+  @characters = Character.create(name: Faker::Movies::HarryPotter.character, location: Faker::Movies::HarryPotter.location, house: Faker::Movies::HarryPotter.house)
+  print @characters
 end
